86cf3700c238abeb2516511fd68f8d51b62ff19a
Rajiv S. Raghavan <rajivsr@yahoo.com>
2016-03-30 21:46:29 -0400

    Changed the receiver address

--- php/contact_form_handler.php
+++ php/contact_form_handler.php
@@ -2,15 +2,15 @@
 	/*From: rajivsr2309@gator4261.hostgator.com*/
 	$name=$_POST['name'];
 	$email_address = $_POST['email'];
-	$subject='Civic Steel Replies';
+	$subject='Contact Form Replies';
 	$phone=$_POST['phone'];
 	$message=$_POST['msg'];
 	$body = "You have received a new message. ".
 	" Here are the details:\n Name: $name \n ".
 	"Email: $email_address\n Phone Number: $phone \n Message: $message \n ";
 	
-	mail('rajivsr2309@gmail.com,sreesh.reeja@gmail.com', $subject, $body);
-	$url="http://www.civicsteel.com";
+	mail('rajivsr2309@gmail.com', $subject, $body);
+	$url="http://rajivsr23.github.io/";
 	header( "Location: $url" );
 	
 
